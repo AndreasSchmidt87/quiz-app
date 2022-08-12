6fe6e448100c0403ff13b11965df14178ac5e822
AndySch666 <schmidt.hof@web.de>
2022-08-12 16:02:25 +0200

    updated bookmarks

--- js/_bookmark.js
+++ js/_bookmark.js
@@ -1,6 +1,9 @@
 export default function toggleBookmark() {
     const cards = document.querySelectorAll('[data-js="qcards"]');
     cards.forEach((card) => {
+        const bookmarkButtons = card.querySelector('[data-js="bookmark"]');
+        console.log(bookmarkButtons);
+        if ()
         
 });
 }
